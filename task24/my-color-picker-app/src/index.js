56521f9ca4bb0313a087e3d75876252f57f736af
elkhalidifatna <elkhalidifatna@gmail.com>
2024-04-17 19:14:33 +0100

    task24

---
+++ task24/my-color-picker-app/src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import Random from './Random';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <Random/>
+  </React.StrictMode>
+);
+
+
