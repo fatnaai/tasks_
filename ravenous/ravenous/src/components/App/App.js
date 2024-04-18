a7b1a09cc806f0dd01a63942623278049c04f79c
elkhalidifatna <elkhalidifatna@gmail.com>
2024-04-18 19:52:34 +0100

    Task23_ravenous part one

---
+++ ravenous/ravenous/src/components/App/App.js
@@ -0,0 +1,17 @@
+import BusinessList from '../BusinessList/BusinessList';
+import SearchBar from '../SearchBar/SearchBar';
+import React from 'react';
+import './App.css';
+
+class App extends React.Component {
+  render(){
+  return (
+    <div className="App">
+  <h1>ravenous</h1>
+  <SearchBar/>
+  <BusinessList/>
+</div>
+  );
+}
+}
+export default App;
